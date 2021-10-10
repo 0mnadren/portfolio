1792ef69be9b7e07e0f056e00babebee796f0d63
Nemanja Davidović <neuritzerg@gmail.com>
2021-10-10 19:04:31 +0200

    Form validation

--- portfolioApp/static/portfolio/js/script.js
+++ portfolioApp/static/portfolio/js/script.js
@@ -1,5 +1,11 @@
 const themeMode = document.getElementsByClassName('theme-mode');
 const defaultMode = 'dark';
+const inputName = document.getElementById('input-name');
+const inputSubject = document.getElementById('input-subject');
+const inputEmail = document.getElementById('input-email');
+const inputMessage = document.getElementById('input-message');
+const getInTouchForm = document.getElementById('contact-form');
+const submitBtn = document.getElementById('submit-btn');
 
 let theme = localStorage.getItem('theme');
 
@@ -29,3 +35,67 @@ function setTheme(mode) {
 
     localStorage.setItem('theme', mode);
 }
+
+
+const showError = (input, textContent) => {
+    const formDiv = input.parentElement; // .form-div
+    const small = formDiv.querySelector('small');
+    small.innerText = textContent;
+    small.className = 'invalid-input-small visible';
+}
+
+
+const hideError = (input) => {
+    const formDiv = input.parentElement;
+    const small = formDiv.querySelector('small');
+    small.className = 'invalid-input-small invisible';
+}
+
+
+const checkInputs = () => {
+    const inputNameValue = inputName.value.trim();
+    const inputSubjectValue = inputSubject.value.trim();
+    const inputEmailValue = inputEmail.value.trim();
+    const inputMessageValue = inputMessage.value.trim();
+    
+    let validInputs = true;
+
+    if (inputNameValue === null || inputNameValue === '') {
+        showError(inputName, 'Please enter valid name');
+        validInputs = false;
+    } else {
+        hideError(inputName);
+    }
+
+    if (inputSubjectValue === null || inputSubjectValue === '') {
+        showError(inputSubject, 'Please enter valid subject');
+        validInputs = false;
+    } else {
+        hideError(inputSubject);
+    }
+
+    if (inputEmailValue === null || inputEmailValue === '') {
+        showError(inputEmail, 'Please enter valid email');
+        validInputs = false;
+    } else {
+        hideError(inputEmail);
+    }
+
+    if (inputMessageValue === null || inputMessageValue === '') {
+        showError(inputMessage, 'Please enter valid message');
+        validInputs = false;
+    } else {
+        hideError(inputMessage);
+    }
+
+    return validInputs
+}
+
+
+getInTouchForm.addEventListener('submit', (e) => {
+    const validInputs = checkInputs();
+    
+    if (!validInputs) {
+        e.preventDefault();
+    }
+});
